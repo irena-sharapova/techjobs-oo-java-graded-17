e2066752e95ee8cb83b699c1cbb5be9d65e2c1db
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-10-06 00:51:33 -0400

    Task 4 add two tests

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,41 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertNotEquals;
+
 public class JobTest {
+
     //TODO: Create your unit tests here
-}
+    @Test
+    public void testSettingJobId(){
+        Object obj1 = new Object();
+        Object obj2 = new Object();
+
+        assertNotEquals(System.identityHashCode(obj1), System.identityHashCode(obj2), "Object IDs should be distinct");
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields () {
+        Job myJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+       assertTrue(myJob.getName() instanceof String);
+       assertTrue(myJob.getEmployer() instanceof Employer);
+       assertTrue(myJob.getLocation() instanceof Location);
+       assertTrue(myJob.getPositionType() instanceof PositionType);
+       assertTrue(myJob.getCoreCompetency() instanceof CoreCompetency);
+
+        assertEquals("Product tester", myJob.getName());
+        assertEquals("ACME", myJob.getEmployer().getValue());
+        assertEquals("Desert", myJob.getLocation().getValue());
+        assertEquals("Quality control", myJob.getPositionType().getValue());
+        assertEquals("Persistence", myJob.getCoreCompetency().getValue());
+
+    }
+
+
+    }
+
+
